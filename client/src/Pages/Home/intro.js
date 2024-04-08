22d1b23732b3e214f9727f0adebe79e26af3df8c
Aryan Singh <131185434+Aryan-stu@users.noreply.github.com>
2024-04-08 17:20:33 +0530

    Update intro.js

--- client/src/Pages/Home/intro.js
+++ client/src/Pages/Home/intro.js
@@ -5,7 +5,7 @@ import {useSelector} from "react-redux";
 function Intro() {
     const { loading, portfolioData } = useSelector((state) => state.root);
     const {intro} = portfolioData;
-    const {firstName, lastName, welcomeText, description, caption} = intro;
+    const {firstName, lastName, welcomeText, description, caption} = intro ||{};
     return (
         <div className="relative  bg-primary">
           

ca80d06e3feb747e934656f586513e4e4f3bf606
Aryan Singh <131185434+Aryan-stu@users.noreply.github.com>
2024-04-08 15:56:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,7 @@ app.use(express.json());
 app.use("/api/portfolio", portfolioRoute);
 
 const port = process.env.PORT || 5000;
-const path = require("path");
 
-if(process.env.NODE_ENV ==="production"){
-    app.use(express.static(path.join(_dirname, "client/build")));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"client/build/index.html"));
-    });
-}
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });

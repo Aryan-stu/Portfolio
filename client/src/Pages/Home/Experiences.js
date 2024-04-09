2a676cf1614396fb441dbbb719d8095d338fd09a
Aryan Singh <131185434+Aryan-stu@users.noreply.github.com>
2024-04-09 23:11:40 +0530

    Update Experiences.js

--- client/src/Pages/Home/Experiences.js
+++ client/src/Pages/Home/Experiences.js
@@ -12,7 +12,7 @@ function Experiences() {
         <div className="bg-primary">
             <SectionTitle title="Experience" />
             <div className=" flex py-10 gap-20 md:flex-col">
-                <div className="flex flex-col gap-10 border-l-2 border-[#fba66a] w-1/3 md:flex-row md:overflow-x-scroll md:w-full">
+                <div className="flex flex-col gap-10 border-l-2 border-[#fad939] w-1/3 md:flex-row md:overflow-x-scroll md:w-full">
                     {experiences.map((experience, index) => (
                         <div onClick={() => {
                             setSelectedItemIndex(index);
@@ -21,7 +21,7 @@ function Experiences() {
                         >
                             <h1 className={`text-xl px-4 w-full justify-center items-center
                             ${selectedItemIndex === index
-                                    ? 'text-tertiary border-tertiary border-l-4 -ml-[3px] md:ml-0 bg-[#f9741634] py-3 md:w-full'
+                                    ? 'text-tertiary border-tertiary border-l-4 -ml-[3px] md:ml-0 bg-[#fada393c] py-3 md:w-full'
                                     : 'text-white py-3'
                                 }`}
                             >

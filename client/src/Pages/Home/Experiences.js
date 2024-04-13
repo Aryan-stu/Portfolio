4cd8732da84b2b21460f6e70cc81998b9c420d96
Aryan Singh <131185434+Aryan-stu@users.noreply.github.com>
2024-04-13 14:37:51 +0530

    Update Experiences.js

--- client/src/Pages/Home/Experiences.js
+++ client/src/Pages/Home/Experiences.js
@@ -35,10 +35,8 @@ function Experiences() {
                 <div className="flex flex-col gap-5">
                     <h1 className="text-secondary text-2xl font-semibold">{experiences[selectedItemIndex].title}</h1>
                     <h1 className="text-tertiary text-xl">{experiences[selectedItemIndex].company}</h1>
-                    <p className="text-white">
-                Finished three Python projects given by the HRs within a given period. Passed on three different criteria with my published packets on the company server.
-Worked on given projects and completed them before 50% of the other interns.
-                </p>
+                    <p className="text-white">{experiences[selectedItemIndex].description}</p>
+
                 </div>
                 
             </div>
